450280b0e6f900319d0dc2db9ffc7b9f04d3ac0d
Imee Cuison <imeetwelve@gmail.com>
2015-04-15 22:55:56 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+  var map = new Datamap({
+      element: document.getElementById('container'),
+      scope: 'usa',
+      fills: {
+            HIGH: '#afafaf',
+            LOW: '#123456',
+            MEDIUM: 'blue',
+            UNKNOWN: 'rgb(0,0,0)',
+            defaultFill: 'green'
+        },
+        data: {
+            SC: {
+                fillKey: 'LOW',
+                numberOfThings: 2002
+            },
+            NY: {
+                fillKey: 'MEDIUM',
+                numberOfThings: 10381
+            }
+        },
+        geographyConfig: {
+            popupTemplate: function(geo, data) {
+                return ['<div class="hoverinfo"><strong>',
+                        'Number of things in ' + geo.properties.name,
+                        ': ' + data.numberOfThings,
+                        '</strong></div>'].join('');
+            }
+        }
+  });
